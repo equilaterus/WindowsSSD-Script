40d948ab5b6d7319abd79a2fed7aeba6b9f078be
Daniel Cañizares Corrales <dacanizares@outlook.com>
2018-07-27 19:25:43 -0500

    Updates and fixes for unit tests

--- test/UnitTests/CommandUI.Tests.ps1
+++ test/UnitTests/CommandUI.Tests.ps1
@@ -2,35 +2,82 @@ Import-Module ./../src/CommandUI
 
 Describe "CommandUI.AskYesNo Unit tests" {
     Context "When answers Yes" {
+        # Prepare
         Mock -ModuleName CommandUI Write-Host {}
         Mock -ModuleName CommandUI Prompt { return 1 }
 
+        # Execute
         $result = AskYesNo -Title 'A Title' -Caption 'A Caption' -Message 'A Message'
 
+        # Validate
         It "returns true" {
             $result | Should Be $true
         }
     }
 
     Context "When answers No" {
-        Mock -ModuleName CommandUI Write-Host {}
+        # Prepare
+        Mock -ModuleName CommandUI Write-Host { }
         Mock -ModuleName CommandUI Prompt { return 0 }
 
+        # Execute
         $result = AskYesNo -Title 'A Title' -Caption 'A Caption' -Message 'A Message'
 
+        # Validate
         It "returns false" {
             $result | Should Be $false
         }
     }
 
     Context "When answers Anything" {
-        Mock -ModuleName CommandUI Write-Host {}
+        # Prepare
+        Mock -ModuleName CommandUI Write-Host { }
         Mock -ModuleName CommandUI Prompt { return 0451 }
 
+        # Execute
         $result = AskYesNo -Title 'A Title' -Caption 'A Caption' -Message 'A Message'
 
+        # Validate
         It "returns false" {
             $result | Should Be $false
+        }
+    }
+
+    Context "When does not have a title" {
+        # Prepare
+        Mock -ModuleName CommandUI Write-Host { }
+        Mock -ModuleName CommandUI Prompt { return 1 }
+
+        # Execute
+        $result = AskYesNo -Caption 'A Caption' -Message 'A Message'
+
+        # Validate
+        It "write host it is not called" {
+            Assert-MockCalled -ModuleName CommandUI Write-Host -Exactly 0
+        }
+
+        It "returns true" {
+            $result | Should Be $true
+        }
+    }
+
+    Context "When it has a title" {
+        # Prepare
+        $Title = 'The Title'
+        Mock -ModuleName CommandUI Write-Host { } -Verifiable
+        Mock -ModuleName CommandUI Prompt { return 1 }
+
+        # Execute
+        $result = AskYesNo -Title $Title -Caption 'A Caption' -Message 'A Message'
+
+        # Validate
+        It "write host it's called" {
+            Assert-VerifiableMocks
+            Assert-MockCalled -ModuleName CommandUI Write-Host -Exactly 1
+        }
+
+        It "returns true" {
+            $result | Should Be $true
         }
     }
 }
--- test/UnitTests/FolderLinks.Tests.ps1
+++ test/UnitTests/FolderLinks.Tests.ps1
@@ -1,25 +1,47 @@
 Import-Module ./../src/FolderLinks
 
-$OriginPath = '$env:TEMP\origin'
-$DestinationPath = '$env:TEMP\destination'
+$OriginPath = '.\TEMP\origin\'
+$DestinationPath = '.\TEMP\destination\'
+
+function AssertSymbolicLink {
+    # Create link
+    Assert-MockCalled -ModuleName FolderLinks New-Item -ParameterFilter { $Path -eq $OriginPath -and $Value -eq $DestinationPath } -Exactly 1
+
+    # Move origin data
+    Assert-MockCalled -ModuleName FolderLinks Move-Item -ParameterFilter { $Path -eq $($OriginPath + '*') -and $Destination -eq $DestinationPath } -Exactly 1
+    Assert-MockCalled -ModuleName FolderLinks Remove-Item -ParameterFilter { $Path -eq $OriginPath } -Exactly 1
+}
 
 Describe "FolderLinks.LinkFolder Unit Tests" {
-    Context "When Path does not exist" {
-        Mock -ModuleName FolderLinks Test-Path { return $true }
-        Mock -ModuleName FolderLinks Move-Item { }
-        Mock -ModuleName FolderLinks Remove-Item { }
-        Mock -ModuleName FolderLinks New-Item { } -Verifiable -ParameterFilter{ $Path -eq $DestinationPath -or $Path -eq $OriginPath}
+    Context "When DestinationPath does not exist" {
+        # Prepare
+        Mock -ModuleName FolderLinks Test-Path { return $Path -eq $OriginPath }
+        Mock -ModuleName FolderLinks Move-Item { } -Verifiable
+        Mock -ModuleName FolderLinks Remove-Item { } -Verifiable
+        Mock -ModuleName FolderLinks New-Item { } -Verifiable -ParameterFilter { $Path -eq $DestinationPath -or $Path -eq $OriginPath }
 
-        $result = LinkFolder -OriginPath $OriginPath -DestinationPath $DestinationPath -IgnoreExtraFiles $true
+        # Execute
+        $result = LinkFolder -OriginPath $OriginPath -DestinationPath $DestinationPath
 
+        # Validate
         It "returns true" {
             $result | Should Be $true
         }
+
+        It "makes calls correctly" {
+            Assert-VerifiableMocks
+            Assert-MockCalled -ModuleName FolderLinks New-Item -Exactly 2
+            Assert-MockCalled -ModuleName FolderLinks New-Item -ParameterFilter {$Path -eq $DestinationPath } -Exactly 1
+
+            AssertSymbolicLink
+        }
     }
 
-    Context "When Path exists" {
+    Context "When DestinationPath exists" {
+        # Prepare
         Mock -ModuleName FolderLinks Test-Path { return $true }
 
+        # Execute
         $result = LinkFolder -OriginPath $OriginPath -DestinationPath $DestinationPath
 
         It "returns false" {
@@ -27,26 +49,38 @@ Describe "FolderLinks.LinkFolder Unit Tests" {
         }
     }
 
-    Context "When Path exists but IgnoreExtraFiles is true" {
-        Mock -ModuleName FolderLinks Test-Path { return $false }
-        Mock -ModuleName FolderLinks Move-Item { }
-        Mock -ModuleName FolderLinks Remove-Item { }
+    Context "When DestinationPath exists but IgnoreExtraFiles is true" {
+        # Prepare
+        Mock -ModuleName FolderLinks Test-Path { return $true }
+        Mock -ModuleName FolderLinks Move-Item { }  -Verifiable
+        Mock -ModuleName FolderLinks Remove-Item { }  -Verifiable
         Mock -ModuleName FolderLinks New-Item { } -Verifiable -ParameterFilter{ $Path -eq $DestinationPath -or $Path -eq $OriginPath}
 
+        # Execute
         $result = LinkFolder -OriginPath $OriginPath -DestinationPath $DestinationPath -IgnoreExtraFiles $true
 
+        # Validate
         It "returns true" {
             $result | Should Be $true
+        }
+
+        It "makes calls correctly" {
+            Assert-MockCalled -ModuleName FolderLinks New-Item -Exactly 1
+
+            AssertSymbolicLink
         }
     }
 }
 
 Describe "FolderLinks.IsLinked Unit Tests" {
     Context "When normal file" {
+        # Prepare
         Mock -ModuleName FolderLinks Get-Item { }
 
+        # Execute
         $result = IsLinked -Path $OriginPath
 
+        # Validate
         It "returns false" {
             $result | Should Be $false
         }
