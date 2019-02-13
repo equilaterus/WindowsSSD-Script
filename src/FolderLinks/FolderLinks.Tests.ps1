f4f07f35b48f412399852dc7f5d8d9566dc4110d
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-02-12 21:30:10 -0500

    Added missing unit tests for ReLinkFolder

--- src/FolderLinks/FolderLinks.Tests.ps1
+++ src/FolderLinks/FolderLinks.Tests.ps1
@@ -13,6 +13,25 @@ function AssertSymbolicLink {
     Assert-MockCalled -ModuleName FolderLinks Remove-Item -ParameterFilter { $Path -eq $OriginPath } -Exactly 1
 }
 
+function GetItemMocked {
+    Mock -ModuleName FolderLinks Get-Item { 
+        $currentDirectory = [PSCustomObject]@{
+            Value = 'LinkedPath'
+        }
+        $scriptBlock = {
+            return $true;
+        }
+        $memberParam = @{
+            MemberType = "ScriptMethod"
+            InputObject = $currentDirectory
+            Name = "Delete"
+            Value = $scriptBlock
+        }
+        $null = Add-Member @memberParam
+        return $currentDirectory 
+    } -Verifiable -ParameterFilter { $Path -eq $OriginPath }    
+}
+
 Describe 'FolderLinks.LinkFolder Unit Tests' {
     Context 'When DestinationPath does not exist' {
         # Prepare
@@ -69,6 +88,84 @@ Describe 'FolderLinks.LinkFolder Unit Tests' {
             Assert-MockCalled -ModuleName FolderLinks New-Item -Exactly 1
 
             AssertSymbolicLink
+        }
+    }
+}
+
+Describe 'FolderLinks.ReLinkFolder Unit Tests' {
+    Context 'When DestinationPath does not exist' {
+        # Prepare
+        Mock -ModuleName FolderLinks IsLinked { return $true } -Verifiable -ParameterFilter { $Path -eq $OriginPath }
+        Mock -ModuleName FolderLinks Test-Path { return $false } -Verifiable -ParameterFilter { $Path -eq $DestinationPath}
+        Mock -ModuleName FolderLinks New-Item { } -Verifiable -ParameterFilter { $Path -eq $DestinationPath }
+        Mock -ModuleName FolderLinks GetLinkFor { return 'LinkedPath\' } -Verifiable
+        Mock -ModuleName FolderLinks Move-Item { } -Verifiable -ParameterFilter { $Path -eq 'LinkedPath\*' -and $Destination -eq  $DestinationPath }
+        GetItemMocked
+        Mock -ModuleName FolderLinks LinkFolder { return $true } -Verifiable -ParameterFilter { $OriginPath -eq $OriginPath -and $DestinationPath -eq $DestinationPath }
+
+        # Execute
+        $result = ReLinkFolder -OriginPath $OriginPath -DestinationPath $DestinationPath
+
+        # Validate
+        It 'returns true' {
+            $result | Should Be $true
+        }
+
+        It 'makes calls correctly' {
+            Assert-VerifiableMock
+            Assert-MockCalled -ModuleName FolderLinks IsLinked -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks Test-Path -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks New-Item -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks GetLinkFor -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks Move-Item -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks Get-Item -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks LinkFolder -Exactly 1            
+        }
+    }
+
+    Context 'When DestinationPath exists' {
+        # Prepare
+        Mock -ModuleName FolderLinks IsLinked { return $true } -Verifiable -ParameterFilter { $Path -eq $OriginPath }
+        Mock -ModuleName FolderLinks Test-Path { return $true } -Verifiable -ParameterFilter { $Path -eq $DestinationPath}
+        Mock -ModuleName FolderLinks GetLinkFor { return 'LinkedPath\' } -Verifiable
+        Mock -ModuleName FolderLinks Move-Item { } -Verifiable -ParameterFilter { $Path -eq 'LinkedPath\*' -and $Destination -eq  $DestinationPath }
+        GetItemMocked
+        Mock -ModuleName FolderLinks LinkFolder { return $true } -Verifiable -ParameterFilter { $OriginPath -eq $OriginPath -and $DestinationPath -eq $DestinationPath }
+
+        # Execute
+        $result = ReLinkFolder -OriginPath $OriginPath -DestinationPath $DestinationPath
+
+        # Validate
+        It 'returns true' {
+            $result | Should Be $true
+        }
+
+        It 'makes calls correctly' {
+            Assert-VerifiableMock
+            Assert-MockCalled -ModuleName FolderLinks IsLinked -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks Test-Path -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks GetLinkFor -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks Move-Item -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks Get-Item -Exactly 1
+            Assert-MockCalled -ModuleName FolderLinks LinkFolder -Exactly 1            
+        }
+    }
+
+    Context 'When Not IsLinked' {
+        # Prepare
+        Mock -ModuleName FolderLinks IsLinked { return $false } -Verifiable -ParameterFilter { $Path -eq $OriginPath }
+        
+        # Execute
+        $result = ReLinkFolder -OriginPath $OriginPath -DestinationPath $DestinationPath
+
+        # Validate
+        It 'returns false' {
+            $result | Should Be $false
+        }
+
+        It 'makes calls correctly' {
+            Assert-VerifiableMock
+            Assert-MockCalled -ModuleName FolderLinks IsLinked -Exactly 1
         }
     }
 }
