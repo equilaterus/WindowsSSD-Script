f554909109f9120db5d10a16672c469d06a3309f
dacanizares <dacanizares@outlook.com>
2020-08-16 15:24:46 -0500

    Fixed test

--- src/ConfigManager/ConfigManager.Functional.Tests.ps1
+++ src/ConfigManager/ConfigManager.Functional.Tests.ps1
@@ -9,8 +9,8 @@ Describe 'ConfigManager.LoadTasksFromFile Functional tests' {
         $result = LoadTasksFromFile -Path $DestinationPath
 
         # Validate
-        It 'returns 6 tasks' {
-            $result.Length | Should Be 6       
+        It 'returns 7 tasks' {
+            $result.Length | Should Be 7       
         }
     }
 
